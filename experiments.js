8e32edcb2833675ef7e9a9c2904db5e181ec8243
Mocha <mocha2007andcoco@gmail.com>
2020-05-06 16:25:41 -0400

    [screams]

--- experiments.js
+++ experiments.js
@@ -10,6 +10,6 @@ function hypoHyper(n){
 	if (n === 1)
 		return (a, b) => a*b;
 	if (1 < n)
-		return (a, b) => Math.exp(h(n-1)(Math.log(a), Math.log(b)));
-	return (a, b) => Math.log(h(n+1)(Math.exp(a), Math.exp(b)));
+		return (a, b) => Math.exp(hypoHyper(n-1)(Math.log(a), Math.log(b)));
+	return (a, b) => Math.log(hypoHyper(n+1)(Math.exp(a), Math.exp(b)));
 }
