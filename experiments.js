b52418a80d5a803907de850c881c8ce53e7ece1f
Mocha <mocha2007andcoco@gmail.com>
2020-05-06 17:37:04 -0400

    uhhhh
    i totally forgot where i was going to go with this...

--- experiments.js
+++ experiments.js
@@ -13,3 +13,75 @@ function hypoHyper(n){
 		return (a, b) => Math.exp(hypoHyper(n-1)(Math.log(a), Math.log(b)));
 	return (a, b) => Math.log(hypoHyper(n+1)(Math.exp(a), Math.exp(b)));
 }
+
+class AbstractError extends Error {
+	/** @param {string} [message] */
+	constructor(message){
+		super(message)
+	}
+}
+
+class LambdaComponent {
+	constructor(){
+		if (this.constructor === LambdaComponent)
+			throw AbstractError;
+    }
+	/** @param {LambdaComponent} other
+	 * @returns {boolean}
+	*/
+	equals(other){
+		throw AbstractError;
+	}
+}
+
+class LambdaVariable extends LambdaComponent {
+	constructor(){
+		super();
+		this.id = +new Date();
+	}
+	/** @param {LambdaVariable} other */
+	equals(other){
+		return other instanceof LambdaVariable && this.id === other.id;
+	}
+}
+
+class LambdaApplication extends LambdaComponent {
+	/**
+	 * @param {LambdaComponent} left 
+	 * @param {LambdaComponent} right 
+	 */
+	constructor(left, right){
+		super();
+		this.left = left;
+		this.right = right;
+	}
+	/** @param {LambdaApplication} other */
+	equals(other){
+		return other instanceof LambdaApplication &&
+			this.left === other.left &&
+			this.right === other.right;
+	}
+}
+
+class Lambda extends LambdaComponent {
+	/**
+	 * @param {LambdaComponent} input 
+	 * @param {LambdaComponent} ouput 
+	 */
+	constructor(input, ouput){
+		super();
+		this.input = input;
+		this.ouput = ouput;
+	}
+	/** @param {Lambda} other */
+	equals(other){
+		return other instanceof Lambda &&
+			this.input === other.input &&
+			this.ouput === other.ouput;
+	}
+}
+
+const church = {
+	true: x => y => x,
+	false: x => y => y,
+};
